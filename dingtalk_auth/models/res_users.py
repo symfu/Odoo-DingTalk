bc8b81f8c97a58f508e422bf5564f5ab4784e621
banqu <banqu@163.com>
2019-05-08 22:32:42 +0800

    用手机号+@+域名作为员工关联odoo用户登陆名

--- dingtalk_auth/models/res_users.py
+++ dingtalk_auth/models/res_users.py
@@ -13,42 +13,40 @@ class ResUsers(models.Model):
         通过员工创建Odoo用户
         """
         employee = request.env['hr.employee'].sudo().search([('id', '=', employee_id)])
-
-        # 获取不重复的邮箱
-        email = employee.work_email
-        if email:
-            email_str_array = email.split('@')
-            email_name = email_str_array[0]
-            email_host = email_str_array[1]
-            if email_host != 'ongood.cn':
-                # 返回True是因为免登的时候会判断是否注册失败，如果为True则注册失败，重定向到Odoo登陆页面
-                return True
+        if employee:
+            # 账号生成格式：手机号@企业邮箱域名
+            email_name = employee.mobile_phone
+            email_host = 'ongood.cn'
             email_count = len(self.search([('login', 'like', email_name)]).sudo())
             if email_count > 0:
-                email = email_name + str(email_count + 1) + '@' + email_host
-        else:
-            return True
+                user = request.env['res.users'].sudo().search([('login', '=', email_name + '@' + email_host)])
+                values = {
+                'user_id': user.id
+                }
+                employee.write(values)
+            else:
+                email = email_name + '@' + email_host
 
-        # 获取不重复的姓名
-        name = employee.name
-        name_count = len(self.search([('name', 'like', name)]).sudo())
-        if name_count > 0:
-            name = name + str(name_count + 1)
+                # 获取不重复的姓名
+                name = employee.name
+                name_count = len(self.search([('name', 'like', name)]).sudo())
+                if name_count > 0:
+                    name = name + str(name_count + 1)
 
-        # 创建Odoo用户
-        values = {
-            'active': active,
-            "login": email,
-            "password": password,
-            "name": name,
-            'email': email,
-            'groups_id': request.env.ref('base.group_user')
-  
-        }
-        user = self.sudo().create(values)
+                # 创建Odoo用户
+                values = {
+                    'active': active,
+                    "login": email,
+                    "password": password,
+                    "name": name,
+                    'email': employee.work_email,
+                    'groups_id': request.env.ref('base.group_user')
+        
+                }
+                user = self.sudo().create(values)
 
-        # 关联员工与用户
-        values = {
-            'user_id': user.id
-        }
-        employee.write(values)
+                # 关联员工与用户
+                values = {
+                    'user_id': user.id
+                }
+                employee.write(values)
