d5599265466fcde064f2a84b74770e1344386fbf
banqu <banqu@163.com>
2019-05-17 23:12:28 +0800

    fix

--- dingtalk_report/models/report.py
+++ dingtalk_report/models/report.py
@@ -97,18 +97,40 @@ class DingtalkReportList(models.Model):
     report_id = fields.Char(string='日志ID')
     remark = fields.Char(string='备注')
     dept_name = fields.Char(string='部门')
-    image1_url = fields.Char(string='图片1链接')
-    image2_url = fields.Char(string='图片2链接')
+    # image1_url = fields.Char(string='图片1链接')
+    # image2_url = fields.Char(string='图片2链接')
     creator_name = fields.Char(string='日志创建人')
     create_time = fields.Char(string='日志创时间')
     read_num = fields.Char(string='已读数')
-    comment_user_num = fields.Char(string='评论数')
+    comment_num = fields.Char(string='评论数')
+    comment_user_num = fields.Char(string='去重后评论数')
     like_num = fields.Char(string='点赞数')
+    content_ids = fields.One2many(comodel_name='dingtalk.report.list.contents', inverse_name='report_id',
+                               string=u'日志内容列表')
+    receiver_user_ids = fields.One2many(comodel_name='dingtalk.report.list.receivers', inverse_name='report_id',
+                               string=u'接受日志用户列表')
+    follower_user_ids = fields.One2many(comodel_name='dingtalk.report.list.followers', inverse_name='report_id',
+                               string=u'相关用户列表')
+    image_url_ids = fields.One2many(comodel_name='dingtalk.report.list.images', inverse_name='report_id',
+                               string=u'照片列表')
     template_id = fields.Many2one(comodel_name='dingtalk.report.template', string=u'日志模板', required=True)
     company_id = fields.Many2one(comodel_name='res.company',
                                  string=u'公司', default=lambda self: self.env.user.company_id.id)            
             
 
+    @api.multi
+    def update_report(self):
+        """
+        获取日志统计数据
+        :param pid:
+        :param pcode:
+        :return:
+        """
+        self.get_report_statistics()
+        self.get_report_receivers()
+        
+ 
+ 
     @api.multi
     def get_report_statistics(self):
         """
@@ -120,31 +142,77 @@ class DingtalkReportList(models.Model):
         
         url = self.env['ali.dingtalk.system.conf'].search([('key', '=', 'get_report_statistics')]).value
         token = self.env['ali.dingtalk.system.conf'].search([('key', '=', 'token')]).value
-        reports = self.env['dingtalk.report.list'].browse(self._context.get('active_ids',[]))
-        for report in reports:
-            if report:
+        # report_ids = self.env['dingtalk.report.list'].browse(self._context.get('active_ids',[]))
+        data = {
+            'report_id': self.report_id,
+        }
+        headers = {'Content-Type': 'application/json'}
+        try:
+            result = requests.post(url="{}{}".format(url, token), headers=headers, data=json.dumps(data), timeout=5)
+            result = json.loads(result.text)
+            logging.info(">>>获取日志统计数据返回结果{}".format(result))
+            if result.get('errcode') == 0:
+                res = result.get('result')
                 data = {
-                    'report_id': report['report_id'],
-                }
-                headers = {'Content-Type': 'application/json'}
-                try:
-                    result = requests.post(url="{}{}".format(url, token), headers=headers, data=json.dumps(data), timeout=5)
-                    result = json.loads(result.text)
-                    logging.info(">>>获取日志统计数据返回结果{}".format(result))
-                    if result.get('errcode') == 0:
-                        res = result.get('result')
-                        data = {
-                                'read_num': res.get('read_num'),
-                                'comment_user_num': res.get('comment_user_num'),
-                                'like_num': res.get('like_num'),
-                                }
-                        report.write(data)
-                    else:
-                        raise UserError('获取日志统计数据失败，详情为:{}'.format(result.get('errmsg')))
-                except ReadTimeout:
-                    raise UserError("网络连接超时！")
+                        'read_num': res.get('read_num'),
+                        'comment_user_num': res.get('comment_user_num'),
+                        'like_num': res.get('like_num'),
+                        }
+                report_list = self.env['dingtalk.report.list'].search(
+                        [('report_id', '=', self.report_id)])
+                if report_list:
+                    report_list.write(data)
+            else:
+                raise UserError('获取日志统计数据失败，详情为:{}'.format(result.get('errmsg')))
+        except ReadTimeout:
+            raise UserError("网络连接超时！")
         logging.info(">>>获取日志统计数据结束...")  
 
+
+    @api.multi
+    def get_report_receivers(self):
+        """
+        获取日志接收人
+        :param pid:
+        :param pcode:
+        :return:
+        """
+        
+        url = self.env['ali.dingtalk.system.conf'].search([('key', '=', 'get_report_receiver_list')]).value
+        token = self.env['ali.dingtalk.system.conf'].search([('key', '=', 'token')]).value
+        # report_ids = self.env['dingtalk.report.list'].browse(self._context.get('active_ids',[]))
+
+        data = {
+            'report_id': self.report_id,
+        }
+        headers = {'Content-Type': 'application/json'}
+        try:
+            result = requests.post(url="{}{}".format(url, token), headers=headers, data=json.dumps(data), timeout=5)
+            result = json.loads(result.text)
+            logging.info(">>>获取日志统计数据返回结果{}".format(result))
+            if result.get('errcode') == 0:
+                res = result.get('result')
+                userid_list = res.get('userid_list')
+                if userid_list:
+                    for userid in userid_list:
+                        emp = self.env['hr.employee'].sudo().search([('din_id', '=', userid)])
+
+                        data = {
+                                'name': self.report_id,
+                                'emp_id': emp.id,
+                                'report_id': self.id,
+                                }
+                        report_receiver = self.env['dingtalk.report.list.receivers'].search(
+                                [('report_id', '=', self.report_id), ('emp_id', '=', emp.id)])
+                        if report_receiver:
+                            pass
+                        else:
+                            report_receiver.create(data)
+            else:
+                raise UserError('获取日志统计数据失败，详情为:{}'.format(result.get('errmsg')))
+        except ReadTimeout:
+            raise UserError("网络连接超时！")
+        logging.info(">>>获取日志统计数据结束...")  
 
 class DownloadDingtalkList(models.TransientModel):
     _name = 'dingtalk.report.list.download'
@@ -166,6 +234,7 @@ class DownloadDingtalkList(models.TransientModel):
         url = self.env['ali.dingtalk.system.conf'].search([('key', '=', 'get_report_list')]).value
         token = self.env['ali.dingtalk.system.conf'].search([('key', '=', 'token')]).value
         templates = self.env['dingtalk.report.template'].browse(self._context.get('active_ids',[]))
+        print('1111111111111111111111111111111111111',templates)
         for temp in templates:
             if temp:
                 data = {
@@ -193,10 +262,10 @@ class DownloadDingtalkList(models.TransientModel):
                                 'creator_name': report.get('creator_name'),
                                 'create_time': self.get_time_stamp(report.get('create_time')),
                             }
-                            if report.get('images'):
-                                data.update({'image1_url': json.loads(report.get('images')[0]).get('image')})
-                                if len(report.get('images')) >1:
-                                    data.update({'image2_url': json.loads(report.get('images')[1]).get('image')})
+                            # if report.get('images'):
+                            #     data.update({'image1_url': json.loads(report.get('images')[0]).get('image')})
+                            #     if len(report.get('images')) >1:
+                            #         data.update({'image2_url': json.loads(report.get('images')[1]).get('image')})
                             report_list = self.env['dingtalk.report.list'].search(
                                 [('report_id', '=', report.get('report_id'))])
                             if report_list:
@@ -205,25 +274,22 @@ class DownloadDingtalkList(models.TransientModel):
                                 self.env['dingtalk.report.list'].create(data)
                             
                             
-                            report_list = self.env['dingtalk.report.list'].search(
-                                [('report_id', '=', report.get('report_id'))])
-                            
                             #获取日志内容
-                            for content in report['contents']:
-                                data = {
-                                'name': report.get('template_name'),
-                                'report_id': report_list.id,
-                                'report_sort': content.get('sort'),
-                                'report_type': content.get('type'),
-                                'report_key': content.get('key'),
-                                'report_value': content.get('value'),
-                                }
-                                report_content = self.env['dingtalk.report.list.contents'].search(
-                                    [('report_id', '=', report_list.id), ('report_sort', '=', content.get('sort'))])
-                                if report_content:
-                                    report_content.write(data)
-                                else:
-                                    self.env['dingtalk.report.list.contents'].create(data)
+                            content = report['contents']
+                            data = {
+                            'report_id': report.get('report_id'),
+                            'report_field_id': report_list.id,
+                            'report_field_sort': content.get('sort'),
+                            'report_field_type': content.get('type'),
+                            'report_field_key': content.get('key'),
+                            'report_field_value': content.get('value'),
+                            }
+                            report_content = self.env['dingtalk.report.list.contents'].search(
+                                [('report_id', '=', report_list.id), ('report_field_sort', '=', content.get('sort'))])
+                            if report_content:
+                                report_content.write(data)
+                            else:
+                                self.env['dingtalk.report.list.contents'].create(data)
 
                     else:
                         raise UserError('获取日志列表失败，详情为:{}'.format(result.get('errmsg')))
@@ -295,11 +361,11 @@ class DownloadDingtalkList(models.TransientModel):
                                 for content in report['contents']:
                                     data = {
                                     'name': report.get('template_name'),
-                                    'report_id': report_list.id,
-                                    'report_sort': content.get('sort'),
-                                    'report_type': content.get('type'),
-                                    'report_key': content.get('key'),
-                                    'report_value': content.get('value'),
+                                    'report_field_id': report_list.id,
+                                    'report_field_sort': content.get('sort'),
+                                    'report_field_type': content.get('type'),
+                                    'report_field_key': content.get('key'),
+                                    'report_field_value': content.get('value'),
                                     }
                                     report_content = self.env['dingtalk.report.list.contents'].search(
                                         [('report_id', '=', report_list.id), ('report_sort', '=', content.get('sort'))])
@@ -330,14 +396,63 @@ class DownloadDingtalkList(models.TransientModel):
 class DingtalkReportListContents(models.Model):
     _name = 'dingtalk.report.list.contents'
     _description = "日志内容"
-    _rec_name = 'name'
+    _rec_name = 'report_id'
 
-    name = fields.Char(string='日志名', required=True)
-    report_sort = fields.Char(string='排序')
-    report_type = fields.Char(string='类型')
-    report_key = fields.Char(string='日志字段')
-    report_value = fields.Char(string='填写内容')
-    report_id = fields.Many2one(comodel_name='dingtalk.report.list', string=u'日志ID', required=True)
+    # name = fields.Char(string='日志名', required=True)
+    report_field_sort = fields.Char(string='排序')
+    report_field_type = fields.Char(string='类型')
+    report_field_key = fields.Char(string='日志字段')
+    report_field_value = fields.Char(string='填写内容')
+    report_id = fields.Many2one(comodel_name='dingtalk.report.list', string='日志ID', required=True)
 
 
-           
+class DingtalkReportListReceivers(models.Model):
+    _name = 'dingtalk.report.list.receivers'
+    _description = "日志接收人员列表"
+    _rec_name = 'report_id'
+
+    # name = fields.Char(string='日志名', required=True)
+    emp_id = fields.Many2one(comodel_name='hr.employee', string='接收人', required=True)
+    report_id = fields.Many2one(comodel_name='dingtalk.report.list', string='日志ID', required=True)
+
+class DingtalkReportListFollowers(models.Model):
+    _name = 'dingtalk.report.list.followers'
+    _description = "日志相关人员列表"
+    _rec_name = 'report_id'
+
+    # name = fields.Char(string='日志名', required=True)
+    emp_id = fields.Many2one(comodel_name='hr.employee', string='关注者', required=True)
+    report_id = fields.Many2one(comodel_name='dingtalk.report.list', string='日志ID', required=True)
+
+
+class DingtalkReportListComments(models.Model):
+    _name = 'dingtalk.report.list.comments'
+    _description = "日志评论列表"
+    _rec_name = 'report_id'
+
+    # name = fields.Char(string='日志名', required=True)
+    emp_id = fields.Many2one(comodel_name='hr.employee', string='评论人', required=True)
+    report_comment = fields.Char(string='评论内容')
+    report_id = fields.Many2one(comodel_name='dingtalk.report.list', string='日志ID', required=True)
+
+
+class DingtalkReportListImages(models.Model):
+    _name = 'dingtalk.report.list.images'
+    _description = "日志相关图片"
+    _rec_name = 'report_id'
+
+    # name = fields.Char(string='日志名', required=True)
+    image_url = fields.Char(string='图片链接')
+    report_id = fields.Many2one(comodel_name='dingtalk.report.list', string='日志ID', required=True)
+
+# class DingtalkReportListStatistics(models.Model):
+#     _name = 'dingtalk.report.list.statistics'
+#     _description = "日志统计数据"
+#     _rec_name = 'name'
+
+#     name = fields.Char(string='日志名', required=True)
+#     read_num = fields.Char(string='已读数')
+#     comment_num = fields.Char(string='评论数')
+#     comment_user_num = fields.Char(string='去重后评论数')
+#     like_num = fields.Char(string='点赞数')
+#     report_id = fields.Many2one(comodel_name='dingtalk.report.list', string='日志ID', required=True)
